0f0175abfb305764c45ad995331ba098db062cd2
Jan Carlo Perez <carlo.vallejo@citi.com.mc>
2018-04-11 11:38:04 -0500

    se agregaron test

--- src/test/java/com/jcpv/spring/SpringConfigServerApplicationTests.java
+++ src/test/java/com/jcpv/spring/SpringConfigServerApplicationTests.java
@@ -2,15 +2,61 @@ package com.jcpv.spring;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+
+import java.util.Map;
+
+import static org.junit.Assert.assertEquals;
+
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@ContextConfiguration(classes = SpringConfigServerApplication.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class SpringConfigServerApplicationTests {
+
+	@Value("${local.server.port}")
+	private int port = 0;
 
 	@Test
 	public void contextLoads() {
+	}
+
+	@Test
+	public void configurationAvailableDev() {
+		ResponseEntity<Map> entity = new TestRestTemplate().getForEntity(
+				"http://localhost:" + port + "/config-server-client/development", Map.class);
+		assertEquals(HttpStatus.OK, entity.getStatusCode());
+	}
+
+	@Test
+	public void configurationAvailableProd() {
+		ResponseEntity<Map> entity = new TestRestTemplate().getForEntity(
+				"http://localhost:" + port + "/config-server-client/production", Map.class);
+		assertEquals(HttpStatus.OK, entity.getStatusCode());
+	}
+
+	@Test
+	public void configurationAvailablePostDev() {
+		MultiValueMap<String, String> form = new LinkedMultiValueMap<>();
+		ResponseEntity<Map> entity = new TestRestTemplate().postForEntity(
+				"http://localhost:" + port + "/config-server-client/development", form, Map.class);
+		assertEquals(HttpStatus.METHOD_NOT_ALLOWED, entity.getStatusCode());
+	}
+
+	@Test
+	public void configurationAvailablePostProd() {
+		MultiValueMap<String, String> form = new LinkedMultiValueMap<>();
+		ResponseEntity<Map> entity = new TestRestTemplate().postForEntity(
+				"http://localhost:" + port + "/config-server-client/production", form, Map.class);
+		assertEquals(HttpStatus.METHOD_NOT_ALLOWED, entity.getStatusCode());
 	}
 
 }
